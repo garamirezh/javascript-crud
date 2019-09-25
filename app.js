d12b1798892aa99bd4adecd3639430838eec95ed
Andres Ramirez <garh180310@gmail.com>
2019-09-25 11:28:41 -0500

    mi primera version

---
+++ app.js
@@ -0,0 +1,83 @@
+//alert('Works!!');
+//console.log("Hello")
+class Product {
+    constructor(name, price, year) {
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+
+}
+
+class UI {
+    addProduct(product) {
+        const productList = document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="card text-center mb-4">
+                <div class="card-body">
+                    <strong>Product Name</strong> : ${product.name}
+                    <strong>Product Price</strong> : ${product.price}
+                    <strong>Product Year</strong> : ${product.year}
+                    <a hef="#" class="btn btn-danger" name="delete">Delete</a>
+                </div>
+            </div
+        `;
+        productList.appendChild(element);
+        this.resetForm();
+    }
+
+    resetForm() {
+        document.getElementById('product-form').reset();
+    }
+
+    deleteProduct(element) {
+        if (element.name === 'delete') {
+            element.parentElement.parentElement.parentElement.remove();
+            this.showMessage('Product Deleted Successfully', 'success');
+        }
+    }
+
+    showMessage(message, cssClass) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-4`;
+        div.appendChild(document.createTextNode(message));
+        //Showing DOM
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#app');
+        container.insertBefore(div, app);
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+
+}
+
+//Events DOM
+document.getElementById('product-form').addEventListener('submit', function(e) {
+    //alert('Enviando Formulario')
+    const name = document.getElementById('name').value;
+    const price = document.getElementById('price').value;
+    const year = document.getElementById('year').value;
+
+    //console.log(name, price, year);
+
+    const product = new Product(name, price, year);
+
+    const ui = new UI();
+    if (name === '' || price === '' || year === '') {
+        return ui.showMessage('Complete Fields Please', 'danger');
+    }
+    ui.addProduct(product);
+    ui.resetForm();
+    ui.showMessage('Product Added Succesfully', 'success');
+
+    e.preventDefault();
+});
+
+document.getElementById('product-list').addEventListener('click', function(e) {
+    //alert('Deleting');
+    //console.log(e.target); //Disparo
+    const ui = new UI();
+    ui.deleteProduct(e.target);
+})
